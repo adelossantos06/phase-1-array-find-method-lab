cb0330056ae537c575b71ae4ef1b54a35870e5a6
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-07-14 16:58:27 -0500

    completes array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+]
+
+function superbowlWin(record) {
+    for (const items of record) {
+        if (items.result === "W") {
+            return items.year
+        }
+    }
+}
+
+
+
+
+
